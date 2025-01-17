8102ea15609072f9834278fd99f3b23e6c45c147
Mahithavudutha <s566410@nwmissouriedu>
2025-01-17 07:05:52 -0600

    Initial Commit

---
+++ Vudutha_Assignment02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vudutha_Assignment02
+//
+//  Created by Mahitha Vudutha on 1/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
